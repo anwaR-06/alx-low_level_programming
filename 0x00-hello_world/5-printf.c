a204c23277fa613e1aa5892294b4de0ab496597b
Anas <bougdoulanas@gmail.com>
2023-06-16 17:28:46 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Program prints exactly..., followed by a new line
- *Retuen:0
+ * main - A  program prints exactly, followed by a new line.
+ *Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
