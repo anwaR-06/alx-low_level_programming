26c4acdbee09cb25ab03d5e7d6f5e0af34a17a0e
Anas <bougdoulanas@gmail.com>
2023-06-16 17:22:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program prints exactly..., followed by a new line
+ *Retuen:0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
