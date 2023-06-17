675bd264938ff10776382baf15c945fb84cad3f7
Anas <bougdoulanas@gmail.com>
2023-06-16 17:06:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programe print a quote with puts & prinf
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
